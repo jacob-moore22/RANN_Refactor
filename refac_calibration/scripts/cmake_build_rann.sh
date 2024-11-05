1be5a7b3e7d9740027d5c59889402ced79e3c8d7
Jacob Moore <jacoblinleymoore@gmail.com>
2024-11-05 15:28:15 -0600

    ENH: Building with MATAR and Kokkos

---
+++ refac_calibration/scripts/cmake_build_rann.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+
+kokkos_build_type="${1}"
+trilinos="${2}"
+
+cmake_options=(
+    -D CMAKE_PREFIX_PATH="${MATAR_INSTALL_DIR};${KOKKOS_INSTALL_DIR}"
+    -D CMAKE_BUILD_TYPE=Release
+    #-D CMAKE_BUILD_TYPE=Debug
+)
+
+if [ "$kokkos_build_type" = "none" ]; then
+    cmake_options+=(
+        -D KOKKOS=OFF
+    )
+else
+    cmake_options+=(
+        -D KOKKOS=ON
+    )
+fi
+
+if [[ "$kokkos_build_type" = *"mpi"* ]]; then
+    cmake_options+=(
+        -D MPI=ON
+    )
+fi
+
+# Print CMake options for reference
+echo "CMake Options: ${cmake_options[@]}"
+
+# Configure Examples
+cmake "${cmake_options[@]}" -B "${EXAMPLE_BUILD_DIR}" -S "${EXAMPLE_SOURCE_DIR}"
+
+# Build Examples
+make -C "${EXAMPLE_BUILD_DIR}" -j${MATAR_BUILD_CORES}
+
+cd $basedir
---
+++ refac_calibration/scripts/setup-env.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -e
+
+# Initialize variables with default values
+machine="$1"
+kokkos_build_type="$2"
+build_cores="$3"
+
+my_build="build-rann-${kokkos_build_type}"
+
+export scriptdir=`pwd`
+
+cd ..
+export topdir=`pwd`
+export basedir=${topdir}
+# export srcdir=${basedir}/src
+# export libdir=${topdir}/lib
+export builddir=${basedir}/${my_build}
+export installdir=${basedir}/install
+
+export EXAMPLE_SOURCE_DIR=${basedir}
+export EXAMPLE_BUILD_DIR=${builddir}
+
+# export TEST_SOURCE_DIR=${basedir}/test
+# export TEST_BUILD_DIR=${builddir}
+
+# export BENCHMARK_SOURCE_DIR=${basedir}/benchmark
+# export BENCHMARK_INSTALL_DIR=${basedir}/benchmark/build
+# export BENCHMARK_BUILD_DIR=${builddir}
+
+export KOKKOS_SOURCE_DIR=${basedir}/MATAR/src/Kokkos/kokkos
+export KOKKOS_BUILD_DIR=${builddir}/kokkos
+export KOKKOS_INSTALL_DIR=${installdir}/kokkos
+
+export MATAR_SOURCE_DIR=${basedir}/MATAR
+export MATAR_BUILD_DIR=${builddir}/matar
+export MATAR_INSTALL_DIR=${installdir}/matar
+
+export MATAR_BUILD_CORES=$build_cores
+
+# Clean stale build and install(s)
+
+cd $scriptdir
+
+# Call the appropriate script to load modules based on the machine
+# source machines/$machine-env.sh ${2}
+
+
+
